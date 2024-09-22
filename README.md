4c520e64087fc318241180bc2ff56b10c46e1483
Xi Wang <xidulu@gmail.com>
2024-09-22 01:37:01 +0000

    some changes

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 Getting MuP to work on NanoGPT codebase with minimal changes
 
-Use `python est_mup_nano_gpt.py` to perform the coordinate check
+Use `python est_mup_nano_gpt.py` to perform the coordinate check and generate the base shape file (following mup)
+
+Call `bash run.sh` to run some experiments, some quick runs for 5000 iterations give you
+
+![fuyou](results_5000_iter.png)
 
 TODO:
 - Figure out what exactly changes I made
 - Get some results 
+
--- run.sh
+++ run.sh
@@ -2,17 +2,19 @@ seed=1
 base_n_embd=192
 # for n_embd in 48 96 192 384
 # for n_embd in 192 384 768
-for n_embd in 48 96
+for n_embd in 48 96 192 384 768
 do
-for lr in 3.2e-4 6.4e-4 1.28e-3 2.56e-3 5.12e-3 1.024e-2 2.048e-2 4.096e-2 8.192e-2 1.6384e-1 3.2768e-1 6.5536e-1 1.31072e0
+# for lr in 3.2e-4 6.4e-4 1.28e-3 2.56e-3 5.12e-3 1.024e-2 2.048e-2 4.096e-2 8.192e-2 1.6384e-1 3.2768e-1 6.5536e-1 1.31072e0
+for lr in 1.28e-3 2.56e-3 5.12e-3 1.024e-2 2.048e-2 4.096e-2 8.192e-2 1.6384e-1 3.2768e-1 6.5536e-1 1.31072e0
 # for lr in 6.5536e-1 1.31072e0
 # for lr in 2e-5 4e-5 8e-5 1.6e-4 3.2e-4 6.4e-4 1.28e-3 2.56e-3 5.12e-3 1.024e-2 
 do
 ACCUM_STEPS=32
 # max_iters=18000
-max_iters=5000
+max_iters=18000
+# lbatch comes from Laurence's script : )
 lbatch -g 1 -t 24 \
-    -q gpu-preempt --name owt_wd_$lr \
+    -q gpu --name owt_wd_$lr \
     --memory 100 -c 8 --gputype "[l40s|a100]" --cmd \
     python train.py config/train_gpt2_small_block256.py --compile=False \
     --n_embd=$n_embd --base_shape_file=gpt_${base_n_embd}.bsh --base_n_embd=$base_n_embd \
